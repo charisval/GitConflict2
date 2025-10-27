7c3de5af6b43d45e1009d4ecce8e294c95e4a98d
charisval <charischerryberry@gmail>
2025-10-27 10:44:08 -0700

    added say function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
+void say(string s);
+
 int main()
 {
     cout << "Greeting\n";
     cout << "hello world\n";
     return 0;
+}
+
+void say(string s)
+{
+    cout << s << endl;
 }
